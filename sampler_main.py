69a2bfe162156b5fd7fb40d5d28849dd9b2c7d18
FarnazG <farnaz.golnam@gmail.com>
2021-02-01 18:56:45 -0800

    Implement 1D sampler

--- sampler_main.py
+++ sampler_main.py
@@ -10,7 +10,22 @@ def target(sample):
     return target_dis.pdf(sample)
 
 
+#def target2d(sample):
+#    target_dis = stats.gamma(a=3.99)
+#    return target_dis.pdf(sample[0,]) * target_dis.pdf(sample[1,])
+
 def target2d(sample):
     target_dis = stats.gamma(a=3.99).pdf(sample[1,])
     normal_dis = stats.multivariate_normal(0, 1).pdf(sample[0,])
     return target_dis * normal_dis
+
+
+dim = 1
+if dim == 1:
+    sams = np.linspace(-5, 5, 500)
+    plt.plot(sams, target(sams), c='orangeelif dim == 2:
+    X, Y = np.meshgrid(np.linspace(-5, 5, 500), np.linspace(-5, 5, 500))
+    Z = np.array([X, Y])
+    fig, ax = plt.subplots(1, 2, figsize=(12, 3))
+    ax[0].contourf(X, Y, target2d(Z))
+
